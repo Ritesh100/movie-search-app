46d7b1ea54cb964076ff42bef81f8ac0f39cd1a6
SajanKc <sajan1996kc@gmail.com>
2020-08-01 21:54:23 +0545

    Updates

---
+++ precache-manifest.3be970bdf30e486371699a2e46bb53f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb32d723dff246ce8af5ad4b0973052b",
+    "url": "/movie-search-app/index.html"
+  },
+  {
+    "revision": "2223777674f367c98dcc",
+    "url": "/movie-search-app/static/css/main.f8979e6c.chunk.css"
+  },
+  {
+    "revision": "dff850fcd70354887a3f",
+    "url": "/movie-search-app/static/js/2.fb0ef60e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search-app/static/js/2.fb0ef60e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2223777674f367c98dcc",
+    "url": "/movie-search-app/static/js/main.642867c7.chunk.js"
+  },
+  {
+    "revision": "93854698e18f7c10401e",
+    "url": "/movie-search-app/static/js/runtime-main.fb6de319.js"
+  }
+]);
